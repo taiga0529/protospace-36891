3c1a83d0542fc4a5d163a4d4d609244ad61fed06
taiga0529 <teega6101@gmail.com>
2021-10-01 11:16:39 +0900

    マイグレーションファイルの修正

--- db/migrate/20210928050312_create_comments.rb
+++ db/migrate/20210928050312_create_comments.rb
@@ -1,7 +1,7 @@
 class CreateComments < ActiveRecord::Migration[6.0]
   def change
     create_table :comments do |t|
-      t.text :content,#null: false
+      t.text :content
       t.references :user,  foreign_key: true
       t.references :prototype,  foreign_key: true
       t.timestamps
